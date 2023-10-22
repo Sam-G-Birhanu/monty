4e379efd0faa4a949b0d3b8d722e14c1c1162629
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:38:30 +0530

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -48,4 +48,15 @@ void fun_pop(stack_t **head, unsigned int count) {
     free(temp);
 }
 
+void fun_swap(stack_t **head, unsigned int count) {
+    int tmp;
+    if (*head == NULL || (*head)->next == NULL) {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", count);
+        cleanup_and_exit(head, EXIT_FAILURE);
+    }
+    
+    tmp = (*head)->n;
+    (*head)->n = (*head)->next->n;
+    (*head)->next->n = tmp;
+}
 
