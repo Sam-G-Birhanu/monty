c149b1e3bda6d26fc2e10e96405eb3dbee3ed0d5
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 14:34:36 +0530

    Update push.c

--- push.c
+++ push.c
@@ -5,7 +5,7 @@
  * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **head, unsigned int counter)
+void fun_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
 
