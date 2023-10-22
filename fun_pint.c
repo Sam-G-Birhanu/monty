72beab9630245efff7e236f4e1b7ab0596b726bf
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 16:01:46 +0530

    Update fun_pint.c

--- fun_pint.c
+++ fun_pint.c
@@ -3,12 +3,12 @@
 void fun_pint(stack_t **head, unsigned int counter) {
     if (*head == NULL) {
         fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-        cleanup_and_exit(EXIT_FAILURE);
+        cleanup_and_exit(head, EXIT_FAILURE);
     }
     printf("%d\n", (*head)->n);
 }
 
-void cleanup_and_exit(int status) {
+void cleanup_and_exit(stack_t **head, int status) {
     if (bus.file) {
         fclose(bus.file);
     }
