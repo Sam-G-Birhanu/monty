2a6dff040cbc7cfad3d7e6aa7d6f1ce7ead458df
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-22 13:52:24 -0700

    betty fix

--- fun_add.c
+++ fun_add.c
@@ -1,16 +1,18 @@
 #include "monty.h"
 
-void fun_add(stack_t **head, unsigned int count) {
-    stack_t *temp;
-    int result;
-    if (*head == NULL || (*head)->next == NULL) {
-        fprintf(stderr, "L%d: can't add, stack too short\n", count);
-        cleanup_and_exit(head, EXIT_FAILURE);
-    }
+void fun_add(stack_t **head, unsigned int count)
+{
+	stack_t *temp;
+	int result;
 
-    temp = *head;
-    result = temp->n + temp->next->n;
-    temp->next->n = result;
-    *head = (*head)->next;
-    free(temp);
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", count);
+		cleanup_and_exit(head, EXIT_FAILURE);
+	}
+	temp = *head;
+	result = temp->n + temp->next->n;
+	temp->next->n = result;
+	*head = (*head)->next;
+	free(temp);
 }
