9930ea7d861261809a6d46e2d94a9e23decd750a
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 15:10:13 +0530

    Update push.c

--- push.c
+++ push.c
@@ -21,17 +21,17 @@ void fun_push(stack_t **head, unsigned int counter)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
-			free_stack(*head);
+			fun_free_stack(*head);
 			exit(EXIT_FAILURE); }}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		fun_free_stack(*head);
 		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
-		addnode(head, n);
+		fun_addnode(head, n);
 	else
-		addqueue(head, n);
+		fun_addqueue(head, n);
 }
