9a8e02ed69e2d9b9f11b9b55353e4933734a4fa5
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:39:26 +0530

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -60,3 +60,23 @@ void fun_swap(stack_t **head, unsigned int count) {
     (*head)->next->n = tmp;
 }
 
+/**
+ * fun_pall - prints the stack
+ * @head: stack head
+ * @count: no used
+ * Return: no return
+*/
+void fun_pall(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	(void)count;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
