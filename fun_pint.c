0a9513f7bbc641d6a40a27724c1a193f115a6421
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-10-22 13:57:15 -0700

    added

--- fun_pint.c
+++ fun_pint.c
@@ -1,20 +1,24 @@
 #include "monty.h"
-
-void fun_pint(stack_t **head, unsigned int count) {
-    if (*head == NULL) {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", count);
-        cleanup_and_exit(head, EXIT_FAILURE);
-    }
-    printf("%d\n", (*head)->n);
+void fun_pint(stack_t **head, unsigned int count)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		cleanup_and_exit(head, EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
 
-void cleanup_and_exit(stack_t **head, int status) {
-    if (b.file) {
-        fclose(b.file);
-    }
-    if (b.cont) {
-        free(b.cont);
-    }
-    fun_free_stack(*head);
-    exit(status);
+void cleanup_and_exit(stack_t **head, int status)
+{
+	if (b.file)
+	{
+		fclose(b.file);
+	}
+	if (b.cont)
+	{
+		free(b.cont);
+	}
+	fun_free_stack(*head);
+	exit(status);
 }
