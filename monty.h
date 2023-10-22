02b5780cea85fa54445bda493ccb92f989d44a78
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 15:49:22 +0530

    Update monty.h

--- monty.h
+++ monty.h
@@ -47,6 +47,8 @@ typedef struct bus_s
 extern bus_t bus;
 void fun_push(stack_t **head, unsigned int number);
 void fun_pall(stack_t **head, unsigned int number);
+
+void cleanup_and_exit(int status);
 void fun_addnode(stack_t **head, int n);
 void fun_addqueue(stack_t **head, int n);
 void fun_free_stack(stack_t *head);
