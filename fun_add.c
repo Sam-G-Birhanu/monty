808380bc137d4d8fc97de8dfb0d4c545ce254d8b
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 18:26:59 +0530

    Update fun_add.c

--- fun_add.c
+++ fun_add.c
@@ -1,12 +1,13 @@
 #include "monty.h"
 
 void fun_add(stack_t **head, unsigned int counter) {
+    stack_t *temp;
     if (*head == NULL || (*head)->next == NULL) {
         fprintf(stderr, "L%d: can't add, stack too short\n", counter);
         cleanup_and_exit(head, EXIT_FAILURE);
     }
 
-    stack_t *temp = *head;
+    temp = *head;
     int result = temp->n + temp->next->n;
     temp->next->n = result;
     *head = (*head)->next;
