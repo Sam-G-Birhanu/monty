28561e68dd35e0db2eafa32bd61e1af6ff0078f7
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:11:07 +0530

    Update executeProgram.c

--- executeProgram.c
+++ executeProgram.c
@@ -23,7 +23,7 @@ int fun_exec(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
-	bus.arg = strtok(NULL, " \n\t");
+	b.arg = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
