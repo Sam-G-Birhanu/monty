a202a59dd83f4bfe93f50f97ad55117c7eb66041
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:24:09 +0530

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,8 +1,8 @@
 #include "monty.h"
-void fun_pop(stack_t **head, unsigned int counter) {
+void fun_pop(stack_t **head, unsigned int count) {
     stack_t *temp;
     if (*head == NULL) {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+        fprintf(stderr, "L%d: can't pop an empty stack\n", count);
         cleanup_and_exit(head, EXIT_FAILURE);
     }
 
