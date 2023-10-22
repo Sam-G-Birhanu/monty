ae7474cde094c6953eab3c32c2c3bb52a4116397
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 15:32:38 +0530

    Update executeProgram.c

--- executeProgram.c
+++ executeProgram.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-* execute - executes the opcode
+* fun_execute - executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
 * @file: poiner to monty file
