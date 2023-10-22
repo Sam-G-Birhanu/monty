0774a865661956dfb8acb13d0bef58518335b0c4
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:37:39 +0530

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -36,3 +36,16 @@ void fun_push(stack_t **head, unsigned int count)
 		fun_addqueue(head, n);
 }
 
+void fun_pop(stack_t **head, unsigned int count) {
+    stack_t *temp;
+    if (*head == NULL) {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", count);
+        cleanup_and_exit(head, EXIT_FAILURE);
+    }
+
+    temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+
