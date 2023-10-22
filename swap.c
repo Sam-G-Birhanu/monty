0742e759e74c9cf3efb31057ef08afde1ca3fd1f
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 16:35:21 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,12 @@
 #include "monty.h"
 void fun_swap(stack_t **head, unsigned int counter) {
+    int temp;
     if (*head == NULL || (*head)->next == NULL) {
         fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
         cleanup_and_exit(head, EXIT_FAILURE);
     }
-
-    int temp = (*head)->n;
+    
+    temp = (*head)->n;
     (*head)->n = (*head)->next->n;
     (*head)->next->n = temp;
 }
