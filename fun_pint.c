06654efe207d9a228d56c86993482349ff9816cd
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:22:06 +0530

    Update fun_pint.c

--- fun_pint.c
+++ fun_pint.c
@@ -1,8 +1,8 @@
 #include "monty.h"
 
-void fun_pint(stack_t **head, unsigned int counter) {
+void fun_pint(stack_t **head, unsigned int count) {
     if (*head == NULL) {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+        fprintf(stderr, "L%u: can't pint, stack empty\n", count);
         cleanup_and_exit(head, EXIT_FAILURE);
     }
     printf("%d\n", (*head)->n);
