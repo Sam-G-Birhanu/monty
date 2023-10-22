33bdb0b81576890bd64cc45c01361e3432c4d817
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 16:24:12 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,10 @@
+void fun_swap(stack_t **head, unsigned int counter) {
+    if (*head == NULL || (*head)->next == NULL) {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+        cleanup_and_exit(EXIT_FAILURE);
+    }
+
+    int temp = (*head)->n;
+    (*head)->n = (*head)->next->n;
+    (*head)->next->n = temp;
+}
