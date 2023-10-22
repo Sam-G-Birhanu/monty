74c1807a012d44cfab53fbd736c2da72f8ac6723
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-22 23:24:54 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -2,13 +2,13 @@
 /**
  * fun_queue - prints the top
  * @head: stack head
- * @counter: line_number
+ * @count: line_number
  * Return: no return
 */
-void fun_queue(stack_t **head, unsigned int counter)
+void fun_queue(stack_t **head, unsigned int count)
 {
 	(void)head;
-	(void)counter;
+	(void)count;
 	b.lifi = 1;
 }
 
