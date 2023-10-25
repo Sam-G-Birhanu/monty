95665e58bec6c90d20e1184b3d5c443facb89a20
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-10-25 22:20:38 +0530

    Update fun_mod.c

--- fun_mod.c
+++ fun_mod.c
@@ -21,8 +21,8 @@ void fun_mod(stack_t **head, unsigned int counter)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
 		fclose(b.file);
-		free(b.content);
-		free_stack(*head);
+		free(b.cont);
+		fun_free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
